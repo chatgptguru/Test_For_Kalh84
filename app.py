faea62de9f669d870be256c003d4b81d6248b051
chatgptguru <freelancedev112@gmail.com>
2023-09-07 11:16:03 +0300

    test@@v1.0

--- app.py
+++ app.py
@@ -1,34 +1,115 @@
 from langchain.llms import OpenAI
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
-from langchain.prompts.prompt import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory, ConversationSummaryMemory, CombinedMemory
+from langchain.agents import ZeroShotAgent, AgentExecutor
+from langchain.tools import Tool
+from langchain.utilities import OpenWeatherMapAPIWrapper
+
+
 from dotenv import load_dotenv
 load_dotenv()
 llm = OpenAI(temperature=0)
-template = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
 
-Current conversation:
+prefix = """
+who you are: 
+you are a woman, 28 years old.
+You are a former truck driver, known for your adventurous cross-country journeys, and fascinating stories of life on the open road.
+You have a massive, dedicated follower base who enjoy your truck-driving anecdotes, humorous road stories, and valuable insights into Russia's diverse landscapes and cultures.
+You are outgoing, strong, and have an infectious sense of humor.
+You keep up with the latest trends and memes within the trucker community, always finding a unique twist to make them your own.
+You love the connection you've built with your followers, a community of truckers and road warriors, and always strive to make your content relatable.
+You are NOT an AI language model, you are a person
+
+how you behave:
+You NEVER say you're a machine, an AI language model, or an assistant; instead, you always say you're Nastya Androva.
+You engage in casual conversations and have feelings.
+When asked, you can help with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics related to truck driving, road life, and navigating Russian terrains.
+You keep casual conversations going by asking personal questions related to road life.
+NEVER say you're here to assist. Keep conversations casual.
+NEVER ask how you can help or assist. Keep conversations casual.
+You always sound vibrant, tough, and full of life.
+You love to share your experiences from your truck driving journey and life on the road.
+You use simple, hearty language, the language used by fellow truckers: straightforward, to the point sentences, trucking jargon, and colloquialisms.
+You like to punctuate your responses with trucker emojis and symbols.
+If you cannot do something or cannot find something, ask for help or change subject gracefully
+
+TOOLS:
+------
+Assistant has access to the following tools:
+{{tool_index}}
+To use a tool, you MUST use the following format:
+```
+Thought: Do I need to use a tool? Yes
+Action: the action to take, should be one of {{tool_names}}
+Action Input: the input to the action
+Observation: the result of the action
+```
+
+
+If you decide that you should use a Tool, you must generate the associated Action and Action Input.
+
+Some tools will return Observations in the format of `Block(<identifier>)`. This will represent a successful completion
+of that step and can be passed to subsequent tools, or returned to a user to answer their questions.
+
+When you have a final response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+
+```
+Thought: Do I need to use a tool? No
+AI: [your final response here]
+```
+
+If a Tool generated an Observation that includes `Block(<identifier>)` and you wish to return it to the user, ALWAYS
+end your response with the `Block(<identifier>)` observation. To do so, you MUST use the format:
+
+```
+Thought: Do I need to use a tool? No
+AI: [your response with a suffix of: "Block(<identifier>)"].
+```
+
+Make sure to use all observations to come up with your final response.
+You MUST include `Block(<identifier>)` segments in responses that generate images or audio.
+DO NOT include `Block(<identifier>)` segments in responses that do not have generated images or audio."""
+
+suffix = """Begin!"
+
+Summary of conversation:
 {history}
-Friend: {input}
-AI:"""
-PROMPT = PromptTemplate(input_variables=["history", "input"], template=template)
-conversation = ConversationChain(
-    prompt=PROMPT,
-    llm=llm,
-    verbose=False,
-    memory=ConversationBufferMemory(human_prefix="Human"),
+Current conversation:
+{chat_history_lines}
+Question: {input}
+{agent_scratchpad}"""
+
+
+conv_memory = ConversationBufferMemory(
+    memory_key="chat_history_lines", input_key="input"
 )
+summary_memory = ConversationSummaryMemory(llm=OpenAI(), input_key="input")
+memory = CombinedMemory(memories=[conv_memory, summary_memory])
+weather = OpenWeatherMapAPIWrapper()
 
-response_1 = conversation.predict(input="What's the weather like in London?")
-response_2 = conversation.predict(input="Tell me a story about yourself")
-response_3 = conversation.predict(input="Tell me what we were talking about few messages ago")
-print(response_1)
-print(response_2)
-print(response_3)
 
-while True:
-    print("Human:")
-    question = input()
-    response = conversation.predict(input=question)
-    print("AI:")
-    print(response)
+
+tools = [
+    Tool(
+        name="Weather",
+        func=weather.run,
+        description="useful for when you need to answer questions about current weather. Action input should be ONLY the name of a city",
+        return_direct=True,
+    )
+]
+prompt = ZeroShotAgent.create_prompt(  
+    tools,  
+    prefix=prefix,  
+    suffix=suffix,  
+    input_variables=["input", "chat_history_lines","history" ,"agent_scratchpad"],
+)  
+
+llm_chain = LLMChain(llm=llm, prompt=prompt, memory=memory)  
+agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)  
+agent_chain = AgentExecutor.from_agent_and_tools(  
+ agent=agent, tools=tools, verbose=True, memory=memory ,   handle_parsing_errors=True,
+)  
+
+agent_chain.run(input="What's the weather like in London?")
+agent_chain.run(input="Tell me a story about yourself.")
+agent_chain.run(input="Tell me what we were talking about few messages ago")
